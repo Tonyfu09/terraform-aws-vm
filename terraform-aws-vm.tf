fae1696b985345d424396712411cb784be2d5ee5
Tonyfu09 <39818274+Tonyfu09@users.noreply.github.com>
2024-12-10 15:46:10 -0800

    update main tf

--- terraform-aws-vm.tf
+++ terraform-aws-vm.tf
@@ -4,9 +4,44 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+# Create an VPC for IT Departnment
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+# Create an subnet for an webserver
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+# Set an static internal IP address of the for webserver
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+# The EC2 for webserver 01
+resource "aws_instance" "IT-Departnment-WebServer-01" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
   tags = {
     Name = "IT-Departnment-WebServer-01"
   }  
